d3737a891712c9bc1a1ba7d2e50abf17c89ffeb7
Mehmettarhan <87024517+Mehmettarhan@users.noreply.github.com>
2021-07-22 09:56:48 -0500

    added some new info

--- src/day34_LocalDayTime_Wrapper/LocalDateIntro.java
+++ src/day34_LocalDayTime_Wrapper/LocalDateIntro.java
@@ -13,5 +13,33 @@ public class LocalDateIntro {
         LocalDate today = LocalDate.now();
         System.out.println(today);
 
+        System.out.println("----------------------------------------------------------");
+
+        // create array of string to store 5 classmates'namess
+        String[] names= {"Adnan", "Zahra", "Josh", "Emily"};
+
+        LocalDate[] DoB = {LocalDate.of(1990, 12, 31),
+                LocalDate.of(1991, 5, 1),
+                LocalDate.of(1993, 9, 23),
+                LocalDate.of(1995, 11, 22)};
+
+        for (int i = 0; i <names.length ; i++) {
+            System.out.println(names[i]+"' birthday is: "+DoB[i]);
+
+            System.out.println("---------------------------------------------");
+
+            LocalDate person1 =LocalDate.of(1890, 12, 22);
+            LocalDate person2= LocalDate.of(1980, 10, 31);
+
+            if (person1.isBefore(person2)){
+                System.out.println("person1 is older");
+            }else{
+                System.out.println("person2 is older");
+            }
+
+        }
+        System.out.println("-----------------------------------------------");
+
+
     }
 }
