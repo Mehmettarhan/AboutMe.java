fd3d7c69b2a622b396e7a0cdaa495aaec4946067
Mehmettarhan <87024517+Mehmettarhan@users.noreply.github.com>
2021-07-22 15:02:00 -0500

    requirements for a strong password

--- src/day34_LocalDayTime_Wrapper/LocalDateIntro.java
+++ src/day34_LocalDayTime_Wrapper/LocalDateIntro.java
@@ -6,7 +6,7 @@ public class LocalDateIntro {
 
     public static void main(String[] args) {
 
-        LocalDate eid = LocalDate.of (2021, 7, 20);
+        LocalDate eid = LocalDate.of(2021, 7, 20);
 
         System.out.println(eid);
 
@@ -16,29 +16,54 @@ public class LocalDateIntro {
         System.out.println("----------------------------------------------------------");
 
         // create array of string to store 5 classmates'namess
-        String[] names= {"Adnan", "Zahra", "Josh", "Emily"};
+        String[] names = {"Adnan", "Zahra", "Josh", "Emily"};
 
         LocalDate[] DoB = {LocalDate.of(1990, 12, 31),
                 LocalDate.of(1991, 5, 1),
                 LocalDate.of(1993, 9, 23),
                 LocalDate.of(1995, 11, 22)};
 
-        for (int i = 0; i <names.length ; i++) {
-            System.out.println(names[i]+"' birthday is: "+DoB[i]);
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i] + "' birthday is: " + DoB[i]);
 
             System.out.println("---------------------------------------------");
 
-            LocalDate person1 =LocalDate.of(1890, 12, 22);
-            LocalDate person2= LocalDate.of(1980, 10, 31);
+            LocalDate person1 = LocalDate.of(1890, 12, 22);
+            LocalDate person2 = LocalDate.of(1980, 10, 31);
 
-            if (person1.isBefore(person2)){
+            if (person1.isBefore(person2)) {
                 System.out.println("person1 is older");
-            }else{
+            } else {
                 System.out.println("person2 is older");
             }
 
+
+            System.out.println("-----------------------------------------------------");
+
+            boolean r1 = person1.isLeapYear();
+            System.out.println(r1);
+
+            boolean r2 = LocalDate.of(2023, 1, 1).isLeapYear();
+            System.out.println(r2);
+
+            System.out.println("----------------------------------------------");
+
+            LocalDate currentDate = LocalDate.now();
+
+            System.out.println(currentDate);
+
+            LocalDate unitAssessment3 = currentDate.plusDays(14);
+
+            System.out.println(unitAssessment3);
+
+            LocalDate unitAssessment4 = currentDate.plusMonths(6);
+
+            System.out.println(unitAssessment4);
+
+            LocalDate examDate = currentDate.plusWeeks(5);
+
+            System.out.println(examDate);
         }
-        System.out.println("-----------------------------------------------");
 
 
     }
